e6aa540856fea86dc68dd9f73fc18c6acf230cc6
AudreyL713 <2021leea@gmail.com>
2020-05-05 23:47:30 -0700

    test1 php

--- Pages/HomePage/ContactUs.php
+++ Pages/HomePage/ContactUs.php
@@ -1,5 +1,12 @@
 <?php
 
-echo "hello";
+$name = $_POST["name"];
+$school = $_POST["school"];
+$from = $_POST["email"];
+$msg = $_Post["message"];
+
+
+echo "This message is from";
+echo name;
 
 ?>
