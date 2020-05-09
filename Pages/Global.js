8086fce2134b75f1ed02699e6e815a17d2a43745
AudreyL713 <2021leea@gmail.com>
2020-05-09 15:40:54 -0700

    how to use session storage learned! all works! (i think)

--- Pages/Global.js
+++ Pages/Global.js
@@ -1,18 +1,35 @@
-var colorScheme = true; // true is dark mode, false is light mode
-editColorScheme();
+var colorScheme = sessionStorage.getItem("colorScheme"); // true is dark mode, false is light mode
 
 function modeToggle() {
   var checkBox = document.getElementById("toggle");
-  colorScheme = !checkBox.checked;
+  if (checkBox.checked) {
+    if (sessionStorage) {
+      sessionStorage.setItem("colorScheme", "light");
+    } else {
+      colorScheme = "light";
+    }
+  } else {
+    if (sessionStorage) {
+      sessionStorage.setItem("colorScheme", "dark");
+    } else {
+      colorScheme = "dark";
+    }
+  }
   editColorScheme();
 }
 
 function editColorScheme() {
-    if (colorScheme) {
-        document.documentElement.style.setProperty("--default-bg", "black");
-        document.documentElement.style.setProperty("--default-text", "white");
-      } else {
-        document.documentElement.style.setProperty("--default-bg", "white");
-        document.documentElement.style.setProperty("--default-text", "black");
-      }
+  if (sessionStorage) {
+    colorScheme = sessionStorage.getItem("colorScheme");
+  }
+
+  if (colorScheme == "light") {
+    var checkBox = document.getElementById("toggle");
+    checkBox.checked = true;
+    document.documentElement.style.setProperty("--default-bg", "white");
+    document.documentElement.style.setProperty("--default-text", "black");
+  } else {
+    document.documentElement.style.setProperty("--default-bg", "black");
+    document.documentElement.style.setProperty("--default-text", "white");
+  }
 }
